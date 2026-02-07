df1f12adfa2a626faef71983342c77d8359922be
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2026-02-07 20:56:25 +0100

    refacto: use more modern approach to async in trait

--- src/modules/user/service.rs
+++ src/modules/user/service.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use migration::async_trait::async_trait;
 use sea_orm::DatabaseConnection;
 use sea_orm::EntityTrait;
 use tracing::info;
@@ -12,7 +11,7 @@ pub struct UserService {
     db: Arc<DatabaseConnection>,
 }
 
-#[async_trait]
+#[allow(async_fn_in_trait)]
 pub trait Service<T> {
     fn new(db: Arc<DatabaseConnection>) -> Self;
     fn db(&self) -> &DatabaseConnection;
@@ -23,7 +22,6 @@ pub trait Service<T> {
     async fn get_one(&self, id: i32) -> T;
 }
 
-#[async_trait]
 impl Service<UserDto> for UserService {
     fn new(db: Arc<DatabaseConnection>) -> Self {
         Self { db }
