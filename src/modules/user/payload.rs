ca5a53cca7326a396b93bc1b81662c3dc5b7f30e
LucasSanchez82 <08.lucas.sanchez@gmail.com>
2026-02-08 20:47:59 +0100

    feat: email to lower case

--- src/modules/user/payload.rs
+++ src/modules/user/payload.rs
@@ -1,10 +1,15 @@
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
 use validator::Validate;
+
+fn deserialize_lowercase<'de, D: Deserializer<'de>>(deserializer: D) -> Result<String, D::Error> {
+    String::deserialize(deserializer).map(|s| s.to_lowercase())
+}
 
 #[derive(Deserialize, Validate)]
 pub struct CreateUser {
     #[validate(length(min = 1, message = "Name cannot be empty"))]
     pub name: String,
+    #[serde(deserialize_with = "deserialize_lowercase")]
     #[validate(email(message = "Invalid email address"))]
     pub email: String,
     #[validate(length(min = 8, message = "Password must be at least 8 characters"))]
@@ -13,6 +18,7 @@ pub struct CreateUser {
 
 #[derive(Deserialize, Validate)]
 pub struct LoginPayload {
+    #[serde(deserialize_with = "deserialize_lowercase")]
     #[validate(email(message = "Invalid email address"))]
     pub email: String,
     #[validate(length(min = 1, message = "Password cannot be empty"))]
